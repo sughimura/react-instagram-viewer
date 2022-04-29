64ec42210dad033a0f11c82a5f373c259c5c07b9
sughimura <nxsughi@gmail.com>
2022-04-29 13:05:31 +0900

    feat: 画像を表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
