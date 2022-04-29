90de3d7b133bad3f5450c7f7934c14352aa3d971
sughimura <nxsughi@gmail.com>
2022-04-29 14:51:35 +0900

    feat: AppBarを上部に固定する

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <AppBar position="fixed">
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               News
