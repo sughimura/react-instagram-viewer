15f17c80545fc090d5d78c577dfe7bbebc3605ec
sughimura <nxsughi@gmail.com>
2022-05-01 11:34:06 +0900

    fix: rename REACT_APP_FACEBOOK_APP_ID -> REACT_APP_FACEBOOK_PAGE_APP_ID

--- src/_helpers/init-facebook-sdk.js
+++ src/_helpers/init-facebook-sdk.js
@@ -6,7 +6,7 @@ export function initFacebookSdk() {
     // wait for facebook sdk to initialize before starting the react app
     window.fbAsyncInit = function () {
       window.FB.init({
-        appId: process.env.REACT_APP_FACEBOOK_APP_ID,
+        appId: process.env.REACT_APP_FACEBOOK_PAGE_APP_ID,
         autoLogAppEvents: true,
         xfbml: true,
         version: 'v13.0'
