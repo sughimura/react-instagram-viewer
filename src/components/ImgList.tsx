e71702c168869904502fba97f2fad3bd0f096806
sughimura <nxsughi@gmail.com>
2022-05-01 18:41:47 +0900

    refactor: extract fetchPhotos()

--- src/components/ImgList.tsx
+++ src/components/ImgList.tsx
@@ -17,41 +17,43 @@ export default function ImgList() {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [items, setItems] = useState<Item[]>([]);
 
+  const fetchPhotos = (hashtagId: string) => {
+    FB.api(
+      `/${hashtagId}/top_media`,
+      'get',
+      {
+        "user_id": Config.fbPageUserId,
+        access_token: Config.fbAccessToken,
+        "fields":"id,media_type,media_url,permalink,like_count,comments_count,caption,timestamp,children{id,media_url}"
+      },
+      function(response: any) {
+        if (response.error) {
+          setError({ message: response.error.message });
+          return;
+        }
+        setIsLoaded(true);
+        setItems(
+          response.data.map((post: any): Item => {
+            return {
+              id: post.id,
+              mediaUrl: post.media_url,
+              mediaType: post.media_type,
+              likeCount: post.like_count,
+              commentsCount: post.comments_count,
+              caption: post.caption
+            }
+          })
+        );
+      }
+    );
+  }
+
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
   useEffect(() => {
     const hashMonsteraId = '17843684002004351'
-    setTimeout(()=> {
-      FB.api(
-        `/${hashMonsteraId}/top_media`,
-        'get',
-        {
-          "user_id": Config.fbPageUserId,
-          access_token: Config.fbAccessToken,
-          "fields":"id,media_type,media_url,permalink,like_count,comments_count,caption,timestamp,children{id,media_url}"
-        },
-        function(response: any) {
-          if (response.error) {
-            setError({ message: response.error.message });
-            return;
-          }
-          setIsLoaded(true);
-          setItems(
-            response.data.map((post: any): Item => {
-              return {
-                id: post.id,
-                mediaUrl: post.media_url,
-                mediaType: post.media_type,
-                likeCount: post.like_count,
-                commentsCount: post.comments_count,
-                caption: post.caption
-              }
-            })
-          );
-        }
-      );
-    }, 1000)
+    setTimeout(()=> fetchPhotos(hashMonsteraId), 1000)
   }, [])
 
   if (error) {
